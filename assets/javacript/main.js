f57c693fd863d2bb5e5c94cc0b1af03012896908
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-05-17 19:42:51 +0530

    style-initialize

---
+++ assets/javacript/main.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.multiText', {
+    strings: ['Coder','Fullstack Developer','Software Engineering','Web designer and UX/UI designer'],
+    loop:true,
+    typeSpeed: 80,
+    delay:1500
+});
