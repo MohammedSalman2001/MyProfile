27ec4f3218d47327e35f6497fc97dafd3be475f6
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-06-07 03:04:50 +0530

    navBarresponse

--- assets/javacript/main.js
+++ assets/javacript/main.js
@@ -4,3 +4,17 @@ var typed = new Typed('.multiText', {
     typeSpeed: 80,
     delay:1500
 });
+
+const hamburger =document.querySelector(".hamburger");
+const navMenu =document.querySelector(".nav-menu")
+
+
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",()=>{
+    hamburger.classList.remove("active")
+    navMenu.classList.remove("active")
+}))
